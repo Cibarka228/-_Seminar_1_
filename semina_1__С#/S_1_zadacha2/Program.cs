27c8c5d474d0b779fceb37f334525c2d86f0656e
Kirill <kirill.paliy003mail.ru>
2023-01-20 18:07:00 +0500

    first commit

---
+++ semina_1__С#/S_1_zadacha2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+int max = 0;
+Console.WriteLine("вводим первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("вводим второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("вводим третье число");
+int threeNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    max = (firstNumber);
+}
+else
+{
+    max = (secondNumber);
+}
+
+if (max > threeNumber)
+{
+    max = (max);
+}
+else
+{
+    max = (threeNumber);
+}
+
+Console.WriteLine($"max =  {max}");
---
+++ semina_1__С#/S_1_zadacha3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.WriteLine("вводим число");
+int Numbers = Convert.ToInt32(Console.ReadLine());
+int remDiv = (Numbers % 2);
+
+if (remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
